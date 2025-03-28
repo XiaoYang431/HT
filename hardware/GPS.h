443147056ca53324892f0c9c45c923d097f1c5a4
huangjunyao123 <148626538+Yao12345312@users.noreply.github.com>
2025-03-28 18:09:40 +0800

    Update GPS.h

--- hardware/GPS.h
+++ hardware/GPS.h
@@ -2,34 +2,25 @@
 #define GPS_H
 
 #define  GPS_RX_PINGroup  GPIO_PA
-#define  GPS_RX_PIN       GPIO_PIN_3
+#define  GPS_RX_PIN       GPIO_PIN_10
 #define  GPS_TX_PINGroup  GPIO_PA
-#define  GPS_TX_PIN       GPIO_PIN_2 
-#define  GPS_UART         UART0
-#define  GPS_UART_HT   HT_UART0
+#define  GPS_TX_PIN       GPIO_PIN_8 
+#define  GPS_UART         USART0
+#define  GPS_UART_HT   HT_USART0
 #define  GPS_RX_PINGroup_HT HT_GPIOA
 
 
 
 
-#define  USART_REC_LEN  150 //定义最大接收字节数 150
-#define GPS_Buffer_Length 150//GPS缓存长度
-#define UTCTime_Length 11//UTC时间长度
-#define latitude_Length 11//纬度长度
-#define N_S_Length 2//N/S长度
-#define longitude_Length 12//经度长度
-#define E_W_Length 2 //E/W长度
+#define  USART_REC_LEN  150
+#define GPS_Buffer_Length 150
+#define UTCTime_Length 11
+#define latitude_Length 11
+#define N_S_Length 2
+#define longitude_Length 12
+#define E_W_Length 2 
 #include "string.h"
 
-
-
-/*
-	经纬度换算公式
-纬度：ddmm.mmmm 北纬 2236.9453 22+(36.9453/60)= 22.615755
-经度：dddmm.mmmm 东经 11408.4790 114+(08.4790/60)=114.141317
-
-
-*/
 typedef struct SaveData 
 {
 	char GPS_Buffer[GPS_Buffer_Length];
@@ -42,13 +33,7 @@ typedef struct SaveData
 	char E_W[E_W_Length];		//E/W
 	char isUsefull;		//定位信息是否有效
 } _SaveData;
- extern _SaveData Save_Data;
 
-
-void GPS_Init(void);//GPS初始化
-void parseGpsBuffer(void);//解析获取GPS数据
-void GPS_ReStart(void);//GPS重启
-void GPS_Stop(void);//GPS停止
-void GPS_Set_Start(void);//GPS开始
-void GPS_Save_Config(void);//GPS保存配置
+void GPS_Init(void);
+void parseGpsBuffer(void);
 #endif
