7bd9f2fe565ce53d83f6b5d962c630e9a8062449
世坤 谭 <3249483516@qq.com>
2025-04-11 18:01:22 +0800

    Add new files and update settings in MDK_ARMv5 project

--- hardware/Motor.c
+++ hardware/Motor.c
@@ -2,6 +2,8 @@
 #include "BMP73T104.h"
 #include "Motor.h"
 //使用I2C_MASTER_CH1，引脚为PC4，PC5
+extern int16_t encder_left;
+extern int16_t encder_right;
 uint8_t Motor_Init(void)
 {
     /*
@@ -36,7 +38,7 @@ void Motor_shache(uint8_t Motor_Nam)
 {
  BMP73T104_dcMotorBrake( Motor_Nam);
 }
-void Set_Motor_decode(void)
+void Set_Motor_decode1(void)
 {
 
 
@@ -114,7 +116,7 @@ void Motor_Get_decode_TMInit(void)
     // 清除中断标志位     
     BFTM_ClearFlag(HT_BFTM0);
     // 开启定时器中断
-    //BFTM_IntConfig(HT_BFTM0, ENABLE);
+    BFTM_IntConfig(HT_BFTM0, ENABLE);
     // 使能中断向量
     NVIC_EnableIRQ(BFTM0_IRQn);
     // 启动定时器
@@ -122,18 +124,11 @@ void Motor_Get_decode_TMInit(void)
 
 }
 
-uint16_t PID_Turn(uint8_t gyro_Z)
-{uint8_t Turn_Kd = 15;
-	uint8_t Turn_Kp = 15;
-	uint16_t PWM_out;
-PWM_out = Turn_Kd * gyro_Z ;
-}
 
 
 
 
-
-void Set_Motor_decode1(void)
+void Set_Motor_decode2(void)
 {
 
 
@@ -159,8 +154,7 @@ TM_TimeBaseInitTypeDef TimeBaseIniture;         //结构体
     GPIO_InputConfig(HT_GPIOA, GPIO_PIN_10, ENABLE);       //使能输入功能	 
 	 	 
 	 TM_DeInit(HTCFG_CAP_PORT_decode2A);    //恢复默认设置
-	 TM_TimeBaseStructInit(&TimeBaseIniture);
-	 
+	 TM_TimeBaseStructInit(&TimeBaseIniture);	 
  //对定时器时基以及计数方式初始化
 	 TimeBaseIniture.CounterMode=TM_CNT_MODE_UP;                //边沿对齐向上计数
 	 TimeBaseIniture.CounterReload=768*2*4;                           //计数重装载计数器
@@ -198,68 +192,41 @@ TM_TimeBaseInitTypeDef TimeBaseIniture;         //结构体
    TM_Cmd(HTCFG_CAP_PORT_decode2A,ENABLE);  //使能计数器 
 }
 
-void Set_Motor_decode2(void)
+
+int16_t  PID_Turn(float gzro, int16_t encoder_left,int16_t encoder_right)
 {
-
-
-TM_TimeBaseInitTypeDef TimeBaseIniture;         //结构体
-
-  CKCU_PeripClockConfig_TypeDef CKCUClock = {{0}};
-		
+   uint16_t Kp = 15, Kd = 5; //===PID参数
+    static int16_t bias = 0;
+	int16_t Turn_Amplitude=100, turn, encoder_temp;
 	
-   CKCUClock.Bit.PA         = 1;       //GPIOA时钟打开
-   CKCUClock.Bit.AFIO       = 1;       //复用时钟
-   CKCUClock.Bit.PWM0      = 1;       //开启马达定时器
-   CKCU_PeripClockConfig(CKCUClock, ENABLE);      //时钟使能
-   AFIO_GPxConfig(HTCFG_CAP_GPIO_ID_decode3A, HTCFG_CAP_AFIO_PIN_decode3A, HTCFG_CAP_AFIO_FUN_decode3A);  //开启复用功能
-	 
-	//捕获IO口的设置 
-   CKCUClock.Bit.PA         = 1;       //GPIOA时钟打开
-   CKCUClock.Bit.AFIO       = 1;       //复用时钟
-   CKCUClock.Bit.PWM0      = 1;       //开启马达定时器
-   CKCU_PeripClockConfig(CKCUClock, ENABLE);      //时钟使能
-   AFIO_GPxConfig(HTCFG_CAP_GPIO_ID_decode3B, HTCFG_CAP_AFIO_PIN_decode3B, HTCFG_CAP_AFIO_FUN_decode3B);  //开启复用功能
-		
-	GPIO_InputConfig(HT_GPIOA, GPIO_PIN_14, ENABLE);       //使能输入功能
-    GPIO_InputConfig(HT_GPIOA, GPIO_PIN_10, ENABLE);       //使能输入功能	 
-	 	 
-	 TM_DeInit(HTCFG_CAP_PORT_decode3A);    //恢复默认设置
-	 TM_TimeBaseStructInit(&TimeBaseIniture);
-	 
- //对定时器时基以及计数方式初始化
-	 TimeBaseIniture.CounterMode=TM_CNT_MODE_UP;                //边沿对齐向上计数
-	 TimeBaseIniture.CounterReload=768*2*4;                           //计数重装载计数器
-	 TimeBaseIniture.Prescaler=0;                               //预分频系数
-	 TimeBaseIniture.PSCReloadTime=TM_PSC_RLD_IMMEDIATE;        //立即重装载预分频寄存器
-	 TimeBaseIniture.RepetitionCounter=0;                         //定时器重复寄存器 
-	 TM_TimeBaseInit(HTCFG_CAP_PORT_decode3A, &TimeBaseIniture);
- 
- 
- 
- 
-//     TM_CaptureInitTypeDef  CapInit;
-//
-//    TM_CaptureStructInit(&CapInit);
-//    CapInit.Channel = HTCFG_CAP_CH_decode1A | HTCFG_CAP_CH_decode1B;
-//    CapInit.Polarity = TM_CHP_NONINVERTED;
-//    CapInit.Selection = TM_CHCCS_DIRECT;
-//    CapInit.Prescaler = TM_CHPSC_OFF;
-//    CapInit.Filter = 0x0;
-//    TM_CaptureInit(HTCFG_CAP_PORT_decode1B, &CapInit);
-//  TM_StiConfig(HTCFG_CAP_PORT_decode1B, TM_TRSEL_TI0S0);
-//
-//  /* Slave Mode selection: Trigger Mode                                                                     */
-//  TM_SlaveModeConfig(HTCFG_CAP_PORT_decode1B, TM_SMSEL_STIED);
-//
-
-
-   TM_DecoderConfig(HTCFG_CAP_PORT_decode3A, TM_SMSEL_DECODER3,                   //模式3双边沿计数
-                        TM_CHP_NONINVERTED, TM_CHP_NONINVERTED);   //编码器模式的设置
+	encoder_temp = encoder_left - encoder_right;
+	bias += encoder_temp; //对角速度积分
 	
-   TM_ChExternalClockConfig(HTCFG_CAP_PORT_decode3A, TM_TRSEL_TI0BED, TM_CHP_INVERTED, 6);//比较滤波器
-	//TM_IntConfig(HTCFG_CAP_PORT_decode1A, TM_INT_CH1CC | TM_INT_UEV, ENABLE);//禁用中断
+	//限幅
+	if(bias > Turn_Amplitude)  
+    	bias = Turn_Amplitude;
+	if(bias < -Turn_Amplitude) 
+		bias = -Turn_Amplitude;
+	
+	turn = Kp * bias + Kd * gzro; //===结合Z轴陀螺仪进行PD控制
+	
+	return turn;
 
-   HTCFG_CAP_PORT_decode3A->CNTR=0;      //计数器初始值
-   TM_Cmd(HTCFG_CAP_PORT_decode3A,ENABLE);  //使能计数器 
 }
-
+int16_t velocity(int16_t Targrt_Speed,int16_t encoder_left,int16_t encoder_right,int16_t RC)
+{  
+	static float Velocity,Encoder_Least,Encoder;
+	static float Encoder_Integral;
+	float velocity_KP=-150;//-300;
+	float velocity_KI=-0;//-0.5;	
+   int16_t Targrt= Targrt_Speed;
+	Encoder_Least =(encoder_left-encoder_right)/2-Targrt;                    //===获取最新速度偏差==测量速度（左右编码器之和）-目标速度（此处为零） 
+	Encoder *= 0.7;		                                                //===一阶低通滤波器       
+	Encoder += Encoder_Least*0.3;	                                    //===一阶低通滤波器    
+	Encoder_Integral +=Encoder;                                       //===积分出位移 积分时间：5ms                                     
+	if(Encoder_Integral>15000)  	Encoder_Integral=15000;             //===积分限幅
+	if(Encoder_Integral<-15000)		Encoder_Integral=-15000;          //===积分限幅	
+	Velocity=Encoder*velocity_KP+Encoder_Integral*velocity_KI*RC;        //===速度控制					
+	return Velocity;
+}
+  
--- hardware/Motor.h
+++ hardware/Motor.h
@@ -8,17 +8,18 @@
 
 #define V_IOref 5  //过流保护参考电压
 #define Iset 2   //过流保护电流
-
-
-  #define Motor_decode1A_GPIOX                        A               // D27
-  #define Motor_decode1A_GPION                        4
-  #define  Motor_decode1A_IPN                          GPTM0
-  #define Motor_decode1A_CHN                          0
+/************************************电机一编码器A相****************************************8*/
+#define Motor_decode1A_GPIOX                        A               // D27
+#define Motor_decode1A_GPION                        4
+#define  Motor_decode1A_IPN                          GPTM0
+#define Motor_decode1A_CHN                          0
 #define HTCFG_CAP_GPIO_ID_decode1A                        STRCAT2(GPIO_P,         Motor_decode1A_GPIOX)
 #define HTCFG_CAP_AFIO_PIN_decode1A                        STRCAT2(AFIO_PIN_,      Motor_decode1A_GPION)
 #define HTCFG_CAP_AFIO_FUN_decode1A                        STRCAT2(AFIO_FUN_,       Motor_decode1A_IPN)
 #define HTCFG_CAP_PORT_decode1A                            STRCAT2(HT_,             Motor_decode1A_IPN)
 #define HTCFG_CAP_CH_decode1A                              STRCAT2(TM_CH_,         Motor_decode1A_CHN)
+/************************************电机一编码器A相****************************************8*/
+/************************************电机一编码器B相****************************************8*/
 
   #define Motor_decode1B_GPIOX                        A               // D27
   #define Motor_decode1B_GPION                        5
@@ -29,9 +30,9 @@
 #define HTCFG_CAP_AFIO_FUN_decode1B                        STRCAT2(AFIO_FUN_,       Motor_decode1B_IPN)
 #define HTCFG_CAP_PORT_decode1B                            STRCAT2(HT_,             Motor_decode1B_IPN)
 #define HTCFG_CAP_CH_decode1B                              STRCAT2(TM_CH_,         Motor_decode1B_CHN)
+/************************************电机一编码器B相****************************************8*/
 
-
-
+/************************************电机二编码器A相****************************************8*/
   #define Motor_decode2A_GPIOX                        A               // D27
   #define Motor_decode2A_GPION                        14
   #define  Motor_decode2A_IPN                          MCTM0
@@ -41,47 +42,48 @@
 #define HTCFG_CAP_AFIO_FUN_decode2A                        STRCAT2(AFIO_FUN_,       Motor_decode2A_IPN)
 #define HTCFG_CAP_PORT_decode2A                            STRCAT2(HT_,             Motor_decode2A_IPN)
 #define HTCFG_CAP_CH_decode2A                              STRCAT2(TM_CH_,         Motor_decode2A_CHN)
-
-  #define Motor_decode2B_GPIOX                        A               // D27
-  #define Motor_decode2B_GPION                        10
-  #define  Motor_decode2B_IPN                          MCTM0
-  #define Motor_decode2B_CHN                          1
+/************************************电机二编码器A相****************************************8*/
+/************************************电机二编码器B相****************************************8*/
+#define Motor_decode2B_GPIOX                        A               // D27
+#define Motor_decode2B_GPION                        10
+#define  Motor_decode2B_IPN                          MCTM0
+#define Motor_decode2B_CHN                          1
 #define HTCFG_CAP_GPIO_ID_decode2B                         STRCAT2(GPIO_P,         Motor_decode2B_GPIOX)
 #define HTCFG_CAP_AFIO_PIN_decode2B                        STRCAT2(AFIO_PIN_,      Motor_decode2B_GPION)
 #define HTCFG_CAP_AFIO_FUN_decode2B                        STRCAT2(AFIO_FUN_,       Motor_decode2B_IPN)
 #define HTCFG_CAP_PORT_decode2B                            STRCAT2(HT_,             Motor_decode2B_IPN)
 #define HTCFG_CAP_CH_decode2B                              STRCAT2(TM_CH_,         Motor_decode2B_CHN)
+/************************************电机二编码器B相****************************************8*/
 
 
-  #define Motor_decode3A_GPIOX                        A               // D27
-  #define Motor_decode3A_GPION                        10
-  #define  Motor_decode3A_IPN                          PWM0
-  #define Motor_decode3A_CHN                          0
-#define HTCFG_CAP_GPIO_ID_decode3A                        STRCAT2(GPIO_P,         Motor_decode3A_GPIOX)
-#define HTCFG_CAP_AFIO_PIN_decode3A                        STRCAT2(AFIO_PIN_,      Motor_decode3A_GPION)
-#define HTCFG_CAP_AFIO_FUN_decode3A                        STRCAT2(AFIO_FUN_,       Motor_decode3A_IPN)
-#define HTCFG_CAP_PORT_decode3A                            STRCAT2(HT_,             Motor_decode3A_IPN)
-#define HTCFG_CAP_CH_decode3A                              STRCAT2(TM_CH_,         Motor_decode3A_CHN)
+//   #define Motor_decode3A_GPIOX                        A               // D27
+//   #define Motor_decode3A_GPION                        10
+//   #define  Motor_decode3A_IPN                          PWM0
+//   #define Motor_decode3A_CHN                          0
+// #define HTCFG_CAP_GPIO_ID_decode3A                        STRCAT2(GPIO_P,         Motor_decode3A_GPIOX)
+// #define HTCFG_CAP_AFIO_PIN_decode3A                        STRCAT2(AFIO_PIN_,      Motor_decode3A_GPION)
+// #define HTCFG_CAP_AFIO_FUN_decode3A                        STRCAT2(AFIO_FUN_,       Motor_decode3A_IPN)
+// #define HTCFG_CAP_PORT_decode3A                            STRCAT2(HT_,             Motor_decode3A_IPN)
+// #define HTCFG_CAP_CH_decode3A                              STRCAT2(TM_CH_,         Motor_decode3A_CHN)
 
-  #define Motor_decode3B_GPIOX                        A               // D27
-  #define Motor_decode3B_GPION                        14
-  #define  Motor_decode3B_IPN                          PWM0
-  #define Motor_decode3B_CHN                          1
-#define HTCFG_CAP_GPIO_ID_decode3B                         STRCAT2(GPIO_P,         Motor_decode3B_GPIOX)
-#define HTCFG_CAP_AFIO_PIN_decode3B                        STRCAT2(AFIO_PIN_,      Motor_decode3B_GPION)
-#define HTCFG_CAP_AFIO_FUN_decode3B                        STRCAT2(AFIO_FUN_,       Motor_decode3B_IPN)
-#define HTCFG_CAP_PORT_decode3B                            STRCAT2(HT_,             Motor_decode3B_IPN)
-#define HTCFG_CAP_CH_decode3B                              STRCAT2(TM_CH_,         Motor_decode3B_CHN)
+//   #define Motor_decode3B_GPIOX                        A               // D27
+//   #define Motor_decode3B_GPION                        14
+//   #define  Motor_decode3B_IPN                          PWM0
+//   #define Motor_decode3B_CHN                          1
+// #define HTCFG_CAP_GPIO_ID_decode3B                         STRCAT2(GPIO_P,         Motor_decode3B_GPIOX)
+// #define HTCFG_CAP_AFIO_PIN_decode3B                        STRCAT2(AFIO_PIN_,      Motor_decode3B_GPION)
+// #define HTCFG_CAP_AFIO_FUN_decode3B                        STRCAT2(AFIO_FUN_,       Motor_decode3B_IPN)
+// #define HTCFG_CAP_PORT_decode3B                            STRCAT2(HT_,             Motor_decode3B_IPN)
+// #define HTCFG_CAP_CH_decode3B                              STRCAT2(TM_CH_,         Motor_decode3B_CHN)
 
 
 void Set_Motor_decode1(void);
 void Set_Motor_decode2(void);
-
 uint8_t Motor_Init(void);
 void Motor_Run(uint8_t Motor_Nam,int8_t Speed);
 void Motor_Stop(uint8_t Motor_Nam);
 void Motor_shache(uint8_t Motor_Nam);
 void Motor_Get_decode_TMInit(void);
-void Set_Motor_decode(void);
+
 
 #endif
