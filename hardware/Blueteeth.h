ec71af2de430a981d7817f1b643f8e26595ac65c
huangjunyao123 <148626538+Yao12345312@users.noreply.github.com>
2025-04-12 17:13:33 +0800

    Update Blueteeth.h

--- hardware/Blueteeth.h
+++ hardware/Blueteeth.h
@@ -1,8 +1,11 @@
-#ifndef __USART_H__
-#define __USART_H__
+#ifndef __BLUETEECH_H__
+#define __BLUETEECH_H__
 
 #include "ht32.h"
-void BLUETEETH_USART_Init(void);
-void GPS_To_BlueTooth(void);
 
+void BLUETEETH_Init(void);
+void GPS_To_BlueTooth(void);
+void blueteech_delay(vu32 count);
+uint8_t BM7701_00_1_bleProcess(void);
+void Get_blueteech_status(void);
 #endif
