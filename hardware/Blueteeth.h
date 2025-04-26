1a65f8b5802af528b44ea60e5ded223a2533949b
huangjunyao123 <148626538+Yao12345312@users.noreply.github.com>
2025-04-26 20:39:12 +0800

    Update Blueteeth.h

--- hardware/Blueteeth.h
+++ hardware/Blueteeth.h
@@ -2,18 +2,15 @@
 #define __BLUETEECH_H__
 
 #include "ht32.h"
+#include "gps.h"
 
-typedef struct {
-    double latitude;
-    double longitude;
-    uint8_t valid;
-} GPS_Coordinate;
+
 
 void BLUETEETH_Init(void);
 void GPS_To_BlueTooth(void);
 void blueteech_delay(vu32 count);
 uint8_t BM7701_00_1_bleProcess(void);
 void Get_blueteech_status(void);
-void processBluetoothGPS(void);
-GPS_Coordinate parseGPSMessage(const char* msg);
+void processBluetoothGPS(_SaveData *saveData);
+
 #endif
