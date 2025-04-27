a214e12340bbf3a70e83af9fc8ee3370306de788
huangjunyao123 <148626538+Yao12345312@users.noreply.github.com>
2025-04-27 21:52:08 +0800

    Update SIM900A.h

--- hardware/SIM900A.h
+++ hardware/SIM900A.h
@@ -1,52 +1,31 @@
-#ifndef __SIM900A_H
-#define __SIM900A_H
+#ifndef __SIM900A_H__
+#define __SIM900A_H__
 #include "ht32f5xxxx_01.h"              // Device header
 
 /****************************************************************/
 
 //USE UART3
-#define  SIM_RX_PINGroup  GPIO_PC
-#define  SIM_RX_PIN       GPIO_PIN_15
-#define  SIM_TX_PINGroup  GPIO_PC
-#define  SIM_TX_PIN       GPIO_PIN_14
-#define  SIM_UART         UART3
-#define  SIM_UART_HT   HT_UART3
-#define  SIM_RX_PINGroup_HT HT_GPIOC
+#define  SIM_RX_PINGroup  GPIO_PA
+#define  SIM_RX_PIN       GPIO_PIN_5
+#define  SIM_TX_PINGroup  GPIO_PA
+#define  SIM_TX_PIN       GPIO_PIN_4
+#define  SIM_UART         USART1
+#define  SIM_UART_HT   HT_USART1
+#define  SIM_RX_PINGroup_HT HT_GPIOA
 
 /*用户必改 xxxxxxxxxxx 改成接收短信的电话号码*/
-#define		PHONE_NUMBER		"xxxxxxxxxxx"//接收短信的电话号码
+#define		PHONE_NUMBER		"17707592248"//接收短信的电话号码
 
 
-#define		SIM900A_SEND_AT							"AT\r\n"
-//1.选择短消息格式 （0：PDU模式 1：文本模式）
-#define		SIM900A_SELECT_SMS_FORMAT				"AT+CMGF=1\r\n"			
-//2.选择TE字库集
-#define		SIM900A_SELECT_TE_FONT					"AT+CSCS=\"UCS2\"\r\n"
-//3.保存SMS设置
-#define		SIM900A_SAVE_SMS_SET					"AT+CSCA?\r\n"
-//4.设置短消息文本模式参数
-#define		SIM900A_SET_SMS_TEST_MODE				"AT+CSMP=17,167,0,25\r\n"
 
-extern uint8_t  sim900a_receive_data[128];
-extern uint16_t  sim900a_receive_count;
 
-extern uint8_t verification_code[7];		//存放验证码
+#define RX_BUFFER_SIZE 20
+void SIM_USART_Init(void);
+void SIM_USART_SendString(const char *str);
+void SIM900A_SendCommand(const char *cmd);
+void SIM900A_SendSms(char *phone, char *message);
+void SIM900A_Process(void);
+void SIM900A_makeCall( char *phoneNumber);
 
-void SIM_Usart_sendString(const char *str);
-uint8_t sim900a_send_cmd(uint8_t *str,uint8_t *ack,uint16_t time);	//SIM900A模块发送字符串指令 函数
-void generate_verification_code(char *data,uint8_t number);			// 随机生成验证码（'0'~'9'）
-void ASCII_TO_Unicode(char *ASCII,char *Unicode);					// ASCII 转 unicode
-void SIM900A_ClearBuffer();
-uint8_t sim900a_init(void);
-uint8_t sim900a_send_verification_code(void);
-void SIM900A_delay_ms(vu32 count);
-void SIM900A_delay_us(vu32 count);
-int USART_ReadResponse(HT_USART_TypeDef* USARTx, uint8_t* response, int max_len);
-void SIM900A_SendCHSMS(char *message, char *phonenumber);
-void SIM900A_SendENGSMS(char *message, char *phonenumber);
-int SIM900A_MakeCall(const char* phoneNumber);
-void SIM900A_HangUp(void);
 #endif
-
-
 
